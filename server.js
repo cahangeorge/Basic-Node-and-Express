e84b904ce9bd9c5fafe4d813db0ada0ccbebd460
cahangeorge <cahangeorge@gmail.com>
2021-02-16 20:06:54 +0200

    Learn to be a Programmer

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var bGround = require('fcc-express-bground');
 var myApp = require('./myApp');
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
@@ -28,7 +29,11 @@ app.get("/", function(req, res) {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", function(req, res) {
-  res.json({"message": "Hello json"});
+  if(process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
 });
 
 var port = process.env.PORT || 3000;
