648f6736134a5311e7cad719ec50c804b96ab092
cahangeorge <cahangeorge@gmail.com>
2021-02-24 13:57:45 +0200

    parameter input

--- myApp.js
+++ myApp.js
@@ -10,6 +10,13 @@ app.get('/now',function(req, res, next) {
     res.json({ time: req.time });
   });
 
+app.get('/:word/echo', (req, res, next) => {
+  req.params.word = {echo: word};
+  next();
+}, (req, res) => {
+  res.json(req.params.word);
+});
+
 
 
 
